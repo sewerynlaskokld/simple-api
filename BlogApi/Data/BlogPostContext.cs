6af90f90a80efd94eee8722e263431c3e6c4c305
Seweryn <seweryn.lasko@kldiscovery.com>
2021-05-19 13:45:13 +0200

    Fixes

--- BlogApi/Data/BlogPostContext.cs
+++ BlogApi/Data/BlogPostContext.cs
@@ -19,11 +19,11 @@ namespace BlogApi.Data
 
             builder.Entity<BlogPost>().HasData
             (
-                new BlogPost { Title = "Test 1", Description = "Descrition 1" },
-                new BlogPost { Title = "Test 2", Description = "Descrition 2" },
-                new BlogPost { Title = "Test 3", Description = "Descrition 3" },
-                new BlogPost { Title = "Test 4", Description = "Descrition 4" },
-                new BlogPost { Title = "Test 5", Description = "Descrition 5" }
+                new BlogPost { Id = 1, Title = "Test 1", Description = "Descrition 1" },
+                new BlogPost { Id = 2, Title = "Test 2", Description = "Descrition 2" },
+                new BlogPost { Id = 3, Title = "Test 3", Description = "Descrition 3" },
+                new BlogPost { Id = 4, Title = "Test 4", Description = "Descrition 4" },
+                new BlogPost { Id = 5, Title = "Test 5", Description = "Descrition 5" }
             );
 
         }
--- BlogApi/Startup.cs
+++ BlogApi/Startup.cs
@@ -1,8 +1,12 @@
+using BlogApi.Data;
+using BlogApi.Repository;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 
 namespace BlogApi
 {
@@ -18,7 +22,20 @@ namespace BlogApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            const string connection = @"Data Source=Data/Posts.db";
+            services.AddDbContextPool<BlogPostContext>(opt => opt.UseSqlite(connection));
             services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "Blog Posts API",
+                    Description = "RESTful repository allowing basic CRUD operations on blog post resource."
+                });
+            });
+
+            services.AddScoped<IBlogPostRepository, BlogPostRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
